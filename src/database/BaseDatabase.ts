c0c5124aeee44db6b54fbcd895cbe5ce41105799
gabriel antunes <gabriel.antunes@acad.pucrs.br>
2022-10-03 22:55:02 -0300

    fix database name in connection.ts

--- src/database/BaseDatabase.ts
+++ src/database/BaseDatabase.ts
@@ -9,7 +9,7 @@ export abstract class BaseDatabase{
            port: 3306,
            user: process.env.DB_USER,
            password: process.env.DB_PASSWORD,
-           database: process.env.DB_DATABASE,
+           database: process.env.DB_SCHEMA,
            multipleStatements: true
         },
      });
